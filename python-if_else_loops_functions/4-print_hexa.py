a005bfc5c13d74ff91631dde35dc3f8efc44b0bd
Ali731-Amb <12252@holbertonstudents.com>
2026-01-14 11:33:38 +0100

    Task04

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-for code in range(97, 123):
-    if code != 101 and code != 113:  # 101 est 'e', 113 est 'q'
-        print("{:c}".format(code), end="")
+for i in range(99):
+    print("{:d} = 0x{:x}".format(i, i))
