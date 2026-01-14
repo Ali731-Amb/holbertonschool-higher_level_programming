2dbf4832ee5d2c074e25b800b3e7feeb74e8ce1c
Ali731-Amb <12252@holbertonstudents.com>
2026-01-14 10:58:28 +0100

    Task03

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-# Boucle unique pour parcourir tous les codes ASCII des lettres minuscules
-for code in range(97, 123):  # 'a' à 'z'
-    if code != 101 and code != 113:  # ignorer 'e' et 'q'
-        print(f"{chr(code)}", end="")
+for code in range(97, 123):
+    if code != 101 and code != 113:  # 101 est 'e', 113 est 'q'
+        print("{:c}".format(code), end="")
