6d3f2859ee6c71a86841b9d0091be0cb8b79106e
Ali731-Amb <12252@holbertonstudents.com>
2026-01-14 10:40:37 +0100

    Task02

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-# Boucle unique pour parcourir tous les codes ASCII des lettres minuscules
-for code in range(97, 123):  # 97 = 'a', 122 = 'z'
+for code in range(97, 123):
     print(f"{chr(code)}", end="")
