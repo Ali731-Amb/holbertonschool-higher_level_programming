6e121ff7a4de1d197328fb43db4257ebb536b4f0
Ali731-Amb <12252@holbertonstudents.com>
2026-01-16 00:53:56 +0100

    2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-import sys
+"""Script that prints the number and list of its arguments."""
 
 if __name__ == "__main__":
-    if len(sys.argv) == 1:
-        print("{} arguments.".format((len(sys.argv) - 1)))
-    elif len(sys.argv) == 2:
-        print("{} argument :".format((len(sys.argv) - 1)))
+    import sys
+
+    argc = len(sys.argv) - 1
+
+    if argc == 0:
+        print("0 arguments.")
+    elif argc == 1:
+        print("1 argument:")
     else:
-        print("{} arguments :".format((len(sys.argv) - 1)))
+        print(f"{argc} arguments:")
+
     for i in range(1, len(sys.argv)):
-        print("{} : {}".format((i), sys.argv[i]))
+        print(f"{i}: {sys.argv[i]}")
