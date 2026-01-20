9003653a5aa6da21f47feb766f91c3b514379da1
Ali731-Amb <12252@holbertonstudents.com>
2026-01-21 00:54:56 +0100

    Task00

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -4,6 +4,6 @@ def square_matrix_simple(matrix=[]):
     for i in matrix:
         new_row = []
         for value in i:
-            new_row.append(value **2)
+            new_row.append(value ** 2)
         new_matrix.append(new_row)
     return new_matrix
