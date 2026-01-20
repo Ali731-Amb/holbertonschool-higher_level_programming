b5cda3f2f4a1c0d2dc1310ad956a7757858cdc60
Ali731-Amb <12252@holbertonstudents.com>
2026-01-21 00:50:33 +0100

    Task04

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -2,9 +2,9 @@
 def only_diff_elements(set_1, set_2):
     diff_elements = set()
     for element in set_1:
-        if not element in set_2:
+        if element not in set_2:
             diff_elements.add(element)
     for element in set_2:
-        if not element in set_1:
+        if element not in set_1:
             diff_elements.add(element)
     return diff_elements
