0fe7d94508d694040d3a1523c96f89a978274403
Ali731-Amb <12252@holbertonstudents.com>
2026-01-21 17:11:58 +0100

    Task10

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary or a_dictionary == {}:
+        return None
+    max_score = None
+    best_key = None
+    for key, value in a_dictionary.items():
+        if max_score is None or max_score < value:
+            max_score = value
+            best_key = key
+    return best_key
