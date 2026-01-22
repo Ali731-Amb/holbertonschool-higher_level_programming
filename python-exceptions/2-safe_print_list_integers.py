2dd668651458ea32c6961454310a18e472d1d8f8
Ali731-Amb <12252@holbertonstudents.com>
2026-01-22 21:41:09 +0100

    Task02

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -8,6 +8,6 @@ def safe_print_list_integers(my_list=[], x=0):
         except IndexError:
             break
         except (TypeError, ValueError):
-            pass
+            continue
     print()
     return count
