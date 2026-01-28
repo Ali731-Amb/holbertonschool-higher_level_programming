19da0c3e5045e47f84642f8f0fe7cc716b346a18
Ali731-Amb <12252@holbertonstudents.com>
2026-01-28 20:11:54 +0100

    comment

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+'''class Square that defines a square'''
+
+
 class Square:
+    """
+    Represents a square with a validated size.
+    """
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
