d84813095b70dad80441937af1a0e4886b4a750a
Ali731-Amb <12252@holbertonstudents.com>
2026-02-03 19:34:39 +0100

    Task00

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Module task_00_abc
+This module defines an abstract class hierarchy for animals with an abstract sound method.
+"""
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+    """
+    Abstract class representing an animal.
+    Subclasses must implement the sound method.
+    """
+    @abstractmethod
+    def sound(self):
+        """
+        Abstract method to return the sound of the animal.
+        """
+        pass
+
+class Dog(Animal):
+    """
+    Class representing a dog.
+    """
+    def sound(self):
+        return "Bark"
+
+class Cat(Animal):
+    """
+    Class representing a cat.
+    """
+    def sound(self):
+        return "Meow"
