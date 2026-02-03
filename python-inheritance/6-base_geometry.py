20b986d13c1b05ac396ac00048e50dc799bb8413
Ali731-Amb <12252@holbertonstudents.com>
2026-02-03 11:35:02 +0100

    Task06

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+Add def area
+"""
+
+
+class BaseGeometry:
+    """Base class for geometry shapes."""
+
+    def area(self):
+        """Public instance method that is not yet implemented."""
+        raise Exception("area() is not implemented")
