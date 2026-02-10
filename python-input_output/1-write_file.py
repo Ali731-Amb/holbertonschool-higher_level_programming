add224009e60547856ffc1e287c72f9ba7c281c7
Ali731-Amb <12252@holbertonstudents.com>
2026-02-10 15:47:33 +0100

    Task01

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    Writes a string to a text file (UTF-8) and returns
+    the number of characters written.
+    Args:
+        filename (str): The name of the file to write.
+        text (str): The text to write to the file.
+    Returns:
+        int: The number of characters written.
+"""
+
+
+def write_file(filename="", text=""):
+    with open(filename, 'w', encoding="utf-8") as f:
+        return f.write(text)
