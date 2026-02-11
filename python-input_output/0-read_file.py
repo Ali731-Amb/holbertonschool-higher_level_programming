f2a56d3bfb538a92d97794ae034e4132394b4d8e
Ali731-Amb <12252@holbertonstudents.com>
2026-02-11 10:40:47 +0100

    Task01_Doc

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,6 +2,12 @@
 
 
 def read_file(filename=""):
+    """
+    Lit un fichier texte (UTF-8) et affiche son contenu sur la sortie standard.
+
+    Args:
+        filename (str): Le chemin du fichier à lire. Par défaut, une chaîne vide.
+    """
     with open(filename, 'r', encoding="utf-8") as f:
         for ligne in f:
             print(ligne, end='')
--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
 import json
-"""
-    Creates an object from a JSON file.
-    Args:
-        filename (str): The name of the JSON file to read from.
-    Returns:
-        object: The Python object created from the JSON file.
-"""
+
 
 
 def load_from_json_file(filename):
