6e70204c6d1e3f11b837f63f0b6a1ac318d17410
Ali731-Amb <12252@holbertonstudents.com>
2026-02-11 10:47:43 +0100

    Task00_correct

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-import json
 
 
 def read_file(filename=""):
     """
-    Reads a text file (UTF-8) and prints its content to stdout.
-
+    Reads a text file (UTF-8) and prints its
+    content to stdout.
     Args:
-        filename (str): The path to the file to read. Defaults to an empty string.
+        filename (str): The path to the file to
+        read. Defaults to an empty string.
     """
     with open(filename, 'r', encoding="utf-8") as f:
         for ligne in f:
