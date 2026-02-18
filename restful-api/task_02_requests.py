c68334bb0b4b358d15f95640af837eb82927c87d
Ali731-Amb <12252@holbertonstudents.com>
2026-02-18 18:01:59 +0100

    Task02

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+"""
+Fetch and process posts from a REST API.
+Provides print and CSV export functions.
+"""
+
+import requests
+import csv
+
+
+def fetch_and_print_posts():
+    """
+    Fetch posts and print their titles from the API.
+    """
+    r = requests.get("https://jsonplaceholder.typicode.com/posts")
+    print(f"Status code : {r.status_code}")
+
+    if r.status_code == 200:
+        obj = r.json()
+        for item in r:
+            print(item["Title"])
+
+
+def fetch_and_save_posts():
+    """
+    Fetch posts and save them to a CSV file.
+    """
+    r = requests.get("https://jsonplaceholder.typicode.com/posts")
+    print(f"Status code : {r.status_code}")
+
+    if r.status_code == 200:
+        obj = r.json()
+        p = []
+        for item in r:
+            p = {
+                "id": item["id"],
+                "title": item["title"],
+                "body": item["body"]
+                }
+            p.append(p)
+
+            fieldnames = ["id", "title", "body"]
+        with open("posts.csv", "w", newline="", encoding="utf-8") as f:
+            writer = csv.DictWriter(f, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerows(p)
