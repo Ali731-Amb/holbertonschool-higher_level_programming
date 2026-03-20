3eaa87d6266dd7a488386cfa5b370ad1136bb8e0
Ali731-Amb <12252@holbertonstudents.com>
2026-03-20 19:41:22 +0100

    Task13

---
+++ javascript-warm_up/13-add.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+/**
+ * Fonction add - Additionne deux entiers
+ * @a: Premier entier
+ * @b: Deuxième entier
+ * Return: La somme de a et b
+ */
+exports.add = function (a, b) {
+  return a + b;
+};
