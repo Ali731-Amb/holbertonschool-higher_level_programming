d56957b33ae04ee0fd9dc6e48e8a120ca928a5c9
Ali731-Amb <12252@holbertonstudents.com>
2026-03-25 09:39:48 +0100

    Task00

---
+++ javascript-dom_manipulation/0-script.js
@@ -0,0 +1,4 @@
+// 1. Déclaration d'une constante pour stocker la référence de l'élément
+const header = document.querySelector('header');
+// 2. Manipulation de la propriété de style via la référence stockée
+header.style.color = '#FF0000';
